f63b616c1191726f3959d8baba464a7c57637c55
kasiaekiert <45494643+kasiaekiert@users.noreply.github.com>
2021-11-30 16:22:59 +0100

    Feature/setup employee accounts sprint 1 (#1)
    * Setup devise gem
    
    * Change User in devise to Employee, Create homepage
    
    * Add users to schema, create seeds
    
    * Add Bootrap
    
    * Add navbar buttons to log_in/out/Sign_in
    
    * fixed rubocop
    
    * change devise config.sign_out_via to :delete
    
    * Add Bootrap with webpacker - the proper way
    
    * delete spec for models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(5) do |i|
+  Employee.where(email: "employee#{i}@employee.com").first_or_create! do |employee|
+    employee.password = 'password'
+  end
+end
