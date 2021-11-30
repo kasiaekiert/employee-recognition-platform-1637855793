717c386746e34607d9888cf389f52a8bc987082c
Kasia Ekiert <kasia.ekiert@gmail.com>
2021-11-30 14:06:24 +0100

    fixed rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :employees
 
   resources :users, only: [:index]
-    
-  root to: "pages#home"
+
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
