bb44bea7debfac1274e2e7e491b64426921d1b98
Kasia Ekiert <kasia.ekiert@gmail.com>
2021-11-30 14:03:14 +0100

    Add navbar buttons to log_in/out/Sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :employees
 
   resources :users, only: [:index]
-  
+    
   root to: "pages#home"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
