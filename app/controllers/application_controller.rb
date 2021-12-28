fe5e65d814dca681f199e66f1cb1444f9babe39f
Kasia Ekiert <kasia.ekiert@gmail.com>
2021-12-28 09:29:58 +0100

    refactor of application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    if resource.is_a?(Admin)
-      admin_root_path
-    else
-      root_path
-    end
-  end
 end
