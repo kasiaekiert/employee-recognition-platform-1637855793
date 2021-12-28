c6e061f162fdd565822f09b1dc713e06dcf5c3fa
Kasia Ekiert <kasia.ekiert@gmail.com>
2021-12-28 09:27:51 +0100

    admin panel with diffrent layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path
+    else
+      root_path
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 1.upto(5) do |i|
-  Employee.where(email: "employee#{i}@employee.com").first_or_create! do |employee|
+  Employee.where(email: "employee#{i}@example.com").first_or_create! do |employee|
     employee.password = 'password'
   end
+end
+
+Admin.where(email:"admin@example.com").first_or_create! do |admin|
+  admin.password = 'password'
 end
