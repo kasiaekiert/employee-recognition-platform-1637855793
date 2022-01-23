986335a927fd9583eacccd41343dbb07ccbc15e0
kasiaekiert <45494643+kasiaekiert@users.noreply.github.com>
2022-01-23 20:07:18 +0100

    sprint4/see points (#12)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_091118) do
+ActiveRecord::Schema.define(version: 2022_01_14_082448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2022_01_06_091118) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "available_kudos", default: 5
+    t.integer "earned_points", default: 0
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
