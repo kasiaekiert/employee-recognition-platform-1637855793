138f9e6c2da7aabee26c070444391660c34f8062
Kasia Ekiert <kasia.ekiert@gmail.com>
2022-02-23 18:49:48 +0100

    working rspec

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,11 @@
 class Order < ApplicationRecord
   belongs_to :employee
   belongs_to :reward
-  scope :filter_by_status, -> (status) { where(status: status) }
+  scope :filter_by_status, ->(status) { where(status: status) }
 
   enum status: {
     undelivered: 0,
-    delivered: 1,
+    delivered: 1
   }
 
   def deserialized_reward
