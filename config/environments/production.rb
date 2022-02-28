4aebbc2269d7612715e04a17a7ff99ce6ea07f4b
Kasia Ekiert <kasia.ekiert@gmail.com>
2022-02-28 09:35:27 +0100

    password to email SandGrid chainged

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   ActionMailer::Base.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    password: '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',
+    password: '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',
     domain: 'protected-fortress-07924.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
